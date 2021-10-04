010a584c0599a9f72737d2932e5a0be6f421fe6e
soge-commits <sogent@mail.umkc.edu>
2021-10-04 17:55:18 -0500

    almost completed, just need to open new file then write data to new file

--- lab04.cpp
+++ lab04.cpp
@@ -13,7 +13,7 @@ using namespace std;
 int main(){
     ifstream fin;
     ofstream fout;
-    float inputLine;
+    float inputData;
     float x1, x2, y1, y2, z1, z2;
     float coordinateDistance;
     vector<float>inputCoordinates;
@@ -24,19 +24,34 @@ int main(){
     if(!fin.is_open()){
         cout<<"Could not open input.txt file"<<endl;
         return 1;
+    } else{
+        cout<<"\"input.txt\" file has successfully opened"<<endl;
     }
+    cout<<endl;
 
     cout<<"Reading in x, y and z coordinates"<<endl;
-
-    while(fin>>inputLine){
-        inputCoordinates.push_back(inputLine);
+    while(fin>>inputData){
+        inputCoordinates.push_back(inputData);
 
     }
 
     cout<<"Closing input.txt file"<<endl;
     fin.close();
     cout<<endl;
+
     cout<<"Opening output.txt file"<<endl;
+    fout.open("output.txt");
+    if(!fout.is_open()){
+        cout<<"Could not open \"input.txt\" file"<<endl;
+        return 1;
+    }else{
+        cout<<"\"output.txt\" file has opened successfully"<<endl;
+    }
+
+
+
+
+
 
 
 
