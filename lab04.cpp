fe81f5dcb34fb9b1f597f9a336948e91f56d331e
soge-commits <sogent@mail.umkc.edu>
2021-10-04 20:42:24 -0500

    to fix: try figuring out how to put coordinateDistance into a vector; if not, place fout<<coordinateDistance in for loop (gets distance values into output file successfully) update: went ahead and put calculated coordinates into output.txt file and re-opened it to extract the data to do: might write open and close file functions, current code is cluttered with open/close file code

--- lab04.cpp
+++ lab04.cpp
@@ -12,14 +12,17 @@ using namespace std;
 
 int main(){
     ifstream fin; //initialize inputfile stream
+    ifstream fin2;
     ofstream fout; //initialize outputfile stream
+    ofstream fout2;
     double inputData;
     double x1, x2, y1, y2, z1, z2;
     double coordinateDistance;
     vector<double>inputCoordinates;
     vector<double>outputDistance;
+    vector<double>sortedOutputDistance;
     int i;
-    int numDist=0;
+
     cout<<"Opening \"input.txt\" file"<<endl;
 
     fin.open("/Users/sogent/CLionProjects/lab4-sogent/input.txt");
@@ -40,16 +43,15 @@ int main(){
     fin.close();
     cout<<endl;
 
-    /*
-    cout<<"Opening \"output_sorted.txt\" file"<<endl;
+
+    cout<<"Opening \"output.txt\" file"<<endl;
     fout.open("output.txt");
     if(!fout.is_open()){
-        cout<<"Could not open \"output_sorted.txt\" file"<<endl;
+        cout<<"Could not open \"output.txt\" file"<<endl;
         return 1;
     }else{
-        cout<<"\"output_sorted.txt\" file has opened successfully"<<endl;
+        cout<<"\"output.txt\" file has opened successfully"<<endl;
     }
-     */
 
 
     for(i=0;i<inputCoordinates.size()-1;i+=6) {
@@ -63,24 +65,68 @@ int main(){
         cout << x1 << " " << x2 << " " << y1 << " " << y2 << " " << z1 << " " << z2 << endl;
         coordinateDistance = calcDistance(x1, x2, y1, y2, z1, z2);
         cout<<coordinateDistance<<endl;
+        fout<<coordinateDistance<<endl;
+        /*
         while(i<outputDistance.size()) {
             coordinateDistance = calcDistance(x1, x2, y1, y2, z1, z2);
             outputDistance.push_back(coordinateDistance);
             ++i;
         }
+         */
     }
 
+    cout<<"Closing \"output.txt\" file"<<endl;
+    fout.close();
 
        // MergeSort()
 
-        //fout<<coordinateDistance<<endl;
+    cout<<"Re-opening \"output.txt\" file"<<endl;
+    fin2.open("output.txt");
+    if(!fin2.is_open()){
+        cout<<"Could not open \"output.txt\" file"<<endl;
+        return 1;
+    }else{
+        cout<<"\"output.txt\" file has opened successfully"<<endl;
+    }
 
+    cout<<"Reading in calculated distance coordinates from file"<<endl;
+    while(fin2>>inputData){
+        outputDistance.push_back(inputData);
+    }
 
+    cout<<"Closing \"output.txt\" file"<<endl;
+    fin2.close();
+    cout<<endl;
 
+    for(i=0;i<outputDistance.size();++i){
+        cout<<outputDistance.at(i)<<endl;
+    }
+    cout<<endl;
+    cout<<"Sorted calculated coordinates in ascending order: "<<endl;
+    MergeSort(outputDistance, 0, outputDistance.size()-1);
+    for(i=0;i<outputDistance.size();++i){
+        cout<<outputDistance.at(i)<<endl;
+    }
+
+    cout<<"Opening \"output_sorted.txt\" file"<<endl;
+
+    fout2.open("output_sorted.txt");
+    if(!fout2.is_open()){
+        cout<<"Could not open \"output_sorted.txt\" file"<<endl;
+        return 1;
+    } else{
+        cout<<"\"output_sorted.txt\" file has opened successfully "<<endl;
+    }
+    cout<<endl;
+
+    for(i=0;i<outputDistance.size();++i){
+        cout<<outputDistance.at(i)<<endl;
+        fout2<<outputDistance.at(i)<<endl;
+    }
 
     cout<<"Closing \"output_sorted.txt\" file"<<endl;
-    fout.close();
-
+    fout2.close();
+    cout<<endl;
 
     return 0;
 }
--- mergeSortAlg.cpp
+++ mergeSortAlg.cpp
@@ -7,7 +7,7 @@
 #include <vector>
 using namespace std;
 
-void MergeSortedIntervals(vector<double>& v, int s, int m, int e) {
+void MergeSortedIntervals(vector<double>& myVector, int s, int m, int e) {
 
     // temp is used to temporary store the vector obtained by merging
     // elements from [s to m] and [m+1 to e] in v
@@ -19,40 +19,38 @@ void MergeSortedIntervals(vector<double>& v, int s, int m, int e) {
 
     while (i <= m && j <= e) {
 
-        if (v[i] <= v[j]) {
-            temp.push_back(v[i]);
+        if (myVector[i] <= myVector[j]) {
+            temp.push_back(myVector[i]);
             ++i;
         }
         else {
-            temp.push_back(v[j]);
+            temp.push_back(myVector[j]);
             ++j;
         }
 
     }
 
     while (i <= m) {
-        temp.push_back(v[i]);
+        temp.push_back(myVector[i]);
         ++i;
     }
 
     while (j <= e) {
-        temp.push_back(v[j]);
+        temp.push_back(myVector[j]);
         ++j;
     }
 
     for (int i = s; i <= e; ++i)
-        v[i] = temp[i - s];
+        myVector[i] = temp[i - s];
 
 }
 
-// the MergeSort function
-// Sorts the array in the range [s to e] in v using
-// merge sort algorithm
-void MergeSort(vector<double>& v, int s, int e) {
+
+void MergeSort(vector<double>& myVector, int s, int e) {
     if (s < e) {
         int m = (s + e) / 2;
-        MergeSort(v, s, m);
-        MergeSort(v, m + 1, e);
-        MergeSortedIntervals(v, s, m, e);
+        MergeSort(myVector, s, m);
+        MergeSort(myVector, m + 1, e);
+        MergeSortedIntervals(myVector, s, m, e);
     }
 }
