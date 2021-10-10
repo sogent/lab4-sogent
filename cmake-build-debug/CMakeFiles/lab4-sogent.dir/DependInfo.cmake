ea239dc43a29dd50314dc66ff918f85317e87f7c
soge-commits <sogent@mail.umkc.edu>
2021-10-09 20:21:43 -0500

    to fix: try figuring out how to put coordinateDistance into a vector; if not, place fout<<coordinateDistance in for loop (gets distance values into output file successfully) update: went ahead and put calculated coordinates into output.txt file and re-opened it to extract the data to do: might write open and close file functions, current code is cluttered with open/close file code

---
+++ cmake-build-debug/CMakeFiles/lab4-sogent.dir/DependInfo.cmake
@@ -0,0 +1,31 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/sogent/CLionProjects/lab4-sogent/Lab04.cpp" "/Users/sogent/CLionProjects/lab4-sogent/cmake-build-debug/CMakeFiles/lab4-sogent.dir/Lab04.cpp.o"
+  "/Users/sogent/CLionProjects/lab4-sogent/calc3DDistModule.cpp" "/Users/sogent/CLionProjects/lab4-sogent/cmake-build-debug/CMakeFiles/lab4-sogent.dir/calc3DDistModule.cpp.o"
+  "/Users/sogent/CLionProjects/lab4-sogent/mergeSortAlg.cpp" "/Users/sogent/CLionProjects/lab4-sogent/cmake-build-debug/CMakeFiles/lab4-sogent.dir/mergeSortAlg.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../."
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/lab4-sogent.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/lab4-sogent.dir/Lab04.cpp.o"
+  "CMakeFiles/lab4-sogent.dir/calc3DDistModule.cpp.o"
+  "CMakeFiles/lab4-sogent.dir/mergeSortAlg.cpp.o"
+  "lab4-sogent"
+  "lab4-sogent.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/lab4-sogent.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
